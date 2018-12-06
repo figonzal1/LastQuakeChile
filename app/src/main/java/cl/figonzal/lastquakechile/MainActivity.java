cf0dfee2769488de06c1301be5d0911cf7936b0f
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2018-12-06 01:43:44 -0300

    Avances de java y resources

--- app/src/main/java/cl/figonzal/lastquakechile/MainActivity.java
+++ app/src/main/java/cl/figonzal/lastquakechile/MainActivity.java
@@ -1,13 +1,76 @@
 package cl.figonzal.lastquakechile;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private Toolbar toolbar;
+    private List<QuakeModel> quakeModelList;
+    private RecyclerView rv;
+    private LinearLayoutManager ly;
+    private QuakeAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Buscar toolbar en resources
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+
+        //Setear el toolbar sobre el main activity
+        setSupportActionBar(toolbar);
+
+        rv = findViewById(R.id.recycle_view);
+        rv.setHasFixedSize(true);
+
+        ly = new LinearLayoutManager(this);
+        rv.setLayoutManager(ly);
+
+        quakeModelList = new ArrayList<>();
+
+        QuakeModel model = new QuakeModel();
+        model.setReferencia("Laserena");
+        quakeModelList.add(model);
+        adapter = new QuakeAdapter(quakeModelList);
+        rv.setAdapter(adapter);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menuItem){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar_menu,menuItem);
+        return super.onCreateOptionsMenu(menuItem);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                return true;
+
+            case R.id.settings:
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
