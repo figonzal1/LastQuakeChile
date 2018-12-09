76b744b61fa057cd7acee1fcb341d42abddc49d6
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2018-12-09 20:25:37 -0300

    Card view completo con datos, excepto tiempo de sismo

--- app/src/main/java/cl/figonzal/lastquakechile/QuakeAdapter.java
+++ app/src/main/java/cl/figonzal/lastquakechile/QuakeAdapter.java
@@ -1,5 +1,6 @@
 package cl.figonzal.lastquakechile;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -7,25 +8,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class QuakeAdapter extends RecyclerView.Adapter<QuakeAdapter.QuakeViewHolder> {
 
     private List<QuakeModel> quakeModelList;
+    private Context context;
 
-    QuakeAdapter(List<QuakeModel> quakeModelList){
+    QuakeAdapter(List<QuakeModel> quakeModelList, Context context){
         this.quakeModelList=quakeModelList;
+        this.context =context;
     }
 
 
     static class QuakeViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView tv_nombre;
+        private TextView tv_ciudad;
+        private TextView tv_referencia;
+        private TextView tv_magnitud;
+        private TextView tv_hora;
 
         private QuakeViewHolder(View itemView) {
             super(itemView);
 
-            tv_nombre = itemView.findViewById(R.id.tv_nombre);
+            tv_ciudad = itemView.findViewById(R.id.tv_ciudad);
+            tv_referencia = itemView.findViewById(R.id.tv_referencia);
+            tv_ciudad = itemView.findViewById(R.id.tv_ciudad);
+            tv_magnitud = itemView.findViewById(R.id.tv_magnitud);
+            tv_hora = itemView.findViewById(R.id.tv_hora);
         }
     }
 
@@ -42,6 +54,17 @@ public class QuakeAdapter extends RecyclerView.Adapter<QuakeAdapter.QuakeViewHol
     public void onBindViewHolder(@NonNull QuakeViewHolder holder, int position) {
 
         QuakeModel model = quakeModelList.get(position);
+
+        int inicio = model.getReferencia().indexOf("de")+3;
+        String ciudad = model.getReferencia().substring(inicio,model.getReferencia().length());
+
+        holder.tv_ciudad.setText(ciudad);
+        holder.tv_magnitud.setText(String.format(Locale.US,"%.1f",model.getMagnitud()));
+        holder.tv_referencia.setText(model.getReferencia());
+
+        SimpleDateFormat format = new SimpleDateFormat(context.getString(R.string.TIME_FORMAT), Locale.US);
+        String date = format.format(model.getFecha_local());
+        holder.tv_hora.setText(date);
 
     }
 
