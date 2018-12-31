cc46833886215b99b64a1da10c189602e8e6f71a
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2018-12-31 19:35:22 -0300

    Cambio de colores de sismos

--- app/src/main/java/cl/figonzal/lastquakechile/QuakeUtils.java
+++ app/src/main/java/cl/figonzal/lastquakechile/QuakeUtils.java
@@ -88,12 +88,6 @@ class QuakeUtils {
         int mag_resource_id = 0;
         switch (mag_floor) {
 
-            case 1:
-                mag_resource_id = R.color.magnitude1;
-                break;
-            case 2:
-                mag_resource_id = R.color.magnitude2;
-                break;
             case 3:
                 mag_resource_id = R.color.magnitude3;
                 break;
@@ -113,7 +107,7 @@ class QuakeUtils {
                 mag_resource_id = R.color.magnitude8;
                 break;
             case 9:
-                mag_resource_id = R.color.magnitude9plus;
+                mag_resource_id = R.color.magnitude8;
                 break;
         }
         return mag_resource_id;
