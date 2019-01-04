d14e2fadfa829b03412028318e6e214fb44f8415
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2019-01-04 19:42:30 -0300

    Se corrige imagen de error en glide

--- app/src/main/java/cl/figonzal/lastquakechile/QuakeDetailsActivity.java
+++ app/src/main/java/cl/figonzal/lastquakechile/QuakeDetailsActivity.java
@@ -3,7 +3,6 @@ package cl.figonzal.lastquakechile;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -12,9 +11,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.Locale;
 import java.util.Objects;
@@ -92,11 +93,11 @@ public class QuakeDetailsActivity extends AppCompatActivity {
 
                 switch (escala) {
                     case "Ml":
-                        tv_escala.setText(String.format(getString(R.string.quake_details_escala), "Magnitud Local"));
+                        tv_escala.setText(String.format(getString(R.string.quake_details_escala), "Magnitud Local (Ml)"));
                         break;
 
                     case "Mw":
-                        tv_escala.setText(String.format(getString(R.string.quake_details_escala), "Magnitud Momento"));
+                        tv_escala.setText(String.format(getString(R.string.quake_details_escala), "Magnitud Momento (Mw)"));
                         break;
 
                 }
@@ -123,12 +124,20 @@ public class QuakeDetailsActivity extends AppCompatActivity {
                                     .error(R.drawable.not_found)
                     )
                     .transition(withCrossFade())
-                    .into(new SimpleTarget<Drawable>() {
+                    .listener(new RequestListener<Drawable>() {
                         @Override
-                        public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
-                            iv_mapa.setImageDrawable(resource);
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            iv_mapa.setImageDrawable(getDrawable(R.drawable.not_found));
+                            return false;
                         }
-                    });
+
+                        //No es necesario usarlo (If u want)
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            return false;
+                        }
+                    })
+                    .into(iv_mapa);
         }
     }
 }
