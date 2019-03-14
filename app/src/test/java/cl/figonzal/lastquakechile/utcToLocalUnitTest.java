1a2e0c29ec8c2bb678514aa6a0545a472cd35ac1
figonzal <felipe.gonzalezal.13@sansano.usm.cl>
2019-03-14 16:11:16 -0300

    JUnit test de utilidades

---
+++ app/src/test/java/cl/figonzal/lastquakechile/utcToLocalUnitTest.java
@@ -0,0 +1,63 @@
+package cl.figonzal.lastquakechile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+
+import cl.figonzal.lastquakechile.services.QuakeUtils;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+@RunWith(Parameterized.class)
+public class utcToLocalUnitTest {
+
+    @Parameterized.Parameters(name = "{index}: {0} = utcToLocal({1})")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"2019-03-13 14:00:00", "2019-03-13 11:00:00"}
+                , {"2019-08-09 04:45:00", "2019-08-09 00:45:00"}
+                , {"2020-12-12 15:25:00", "2020-12-12 12:25:00"}
+                , {"2022-12-31 23:59:00", "2022-12-31 20:59:00"}
+                , {"2011-01-01 02:59:00", "2010-12-31 23:59:00"}
+                , {"2020-02-29 02:59:00", "2020-02-28 23:59:00"}
+        });
+    }
+
+    private String actual_utc;
+    private String esperado_local;
+
+    public utcToLocalUnitTest(String actual_utc, String esperado_local) {
+        this.actual_utc = actual_utc;
+        this.esperado_local = esperado_local;
+    }
+
+    @Test
+    public void utcToLocal() {
+
+        Date date_actual = null;
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+
+            //Se testea quakeutils con actual
+            date_actual = QuakeUtils.utcToLocal(format.parse(actual_utc));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        assertEquals(esperado_local, format.format(date_actual));
+
+    }
+
+}
