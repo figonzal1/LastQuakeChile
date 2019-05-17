0672a9583de304e97c5bb682c21d6642dfc3de3d
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2019-05-17 00:42:54 -0400

    Mejora y correcciones de testing que fallaban en dispositivos en español
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- app/src/androidTest/java/cl/figonzal/lastquakechile/StringToDateTest.java
+++ app/src/androidTest/java/cl/figonzal/lastquakechile/StringToDateTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Collection;
@@ -21,23 +22,30 @@ import static junit.framework.TestCase.assertEquals;
 @RunWith(Parameterized.class)
 public class StringToDateTest {
 
-	private String actual, esperada;
+	private String sActual;
+	private Date dEsperada;
 	private Context context;
 
-	public StringToDateTest (String actual, String esperada) {
-		this.actual = actual;
-		this.esperada = esperada;
+	public StringToDateTest (String sActual, Date dEsperada) {
+		this.sActual = sActual;
+		this.dEsperada = dEsperada;
 	}
 
 	@Parameterized.Parameters(name = "{0} = stringToDate({1})")
-	public static Collection<Object[]> data () {
+	public static Collection<Object[]> data () throws ParseException {
 		return Arrays.asList(new Object[][]{
-				{"2019-03-13 14:00:00", "2019-03-13 14:00:00"}
-				, {"2019-08-09 04:45:00", "2019-08-09 04:45:00"}
-				, {"2020-12-12 15:25:00", "2020-12-12 15:25:00"}
-				, {"2022-12-31 23:59:00", "2022-12-31 23:59:00"}
-				, {"2011-01-01 02:59:00", "2011-01-01 02:59:00"}
-				, {"2020-02-29 02:59:00", "2020-02-29 02:59:00"}
+				{"2019-03-13 14:00:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+						.parse("2019-03-13 14:00:00")}
+				, {"2019-08-09 04:45:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+				.parse("2019-08-09 04:45:00")}
+				, {"2020-12-12 15:25:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+				.parse("2020-12-12 15:25:00")}
+				, {"2022-12-31 23:59:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+				.parse("2022-12-31 23:59:00")}
+				, {"2011-01-01 02:59:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+				.parse("2011-01-01 02:59:00")}
+				, {"2020-02-29 02:59:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+				.parse("2020-02-29 02:59:00")}
 		});
 	}
 
@@ -47,11 +55,10 @@ public class StringToDateTest {
 	}
 
 	@Test
-	public void stringToDate () {
+	public void string_to_date () {
 
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date date_actual = QuakeUtils.stringToDate(context, actual);
-		assertEquals(esperada, simpleDateFormat.format(date_actual));
+		Date dActual = QuakeUtils.stringToDate(context, sActual);
+		assertEquals(dEsperada, dActual);
 
 	}
 }
