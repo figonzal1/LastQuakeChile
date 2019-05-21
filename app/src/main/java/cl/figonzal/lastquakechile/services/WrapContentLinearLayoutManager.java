07b03553638cdd298d66d59d5fa0bfc8cfa7cf01
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2019-05-20 23:51:20 -0400

    Correccion error index de recycle view y overlay de detalle se cierra con toque de pantalla
    Signed-off-by: figonzal1 <Felipe.gonzalezalarcon94@gmail.com>

---
+++ app/src/main/java/cl/figonzal/lastquakechile/services/WrapContentLinearLayoutManager.java
@@ -0,0 +1,25 @@
+package cl.figonzal.lastquakechile.services;
+
+import android.content.Context;
+import android.util.Log;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.crashlytics.android.Crashlytics;
+
+public class WrapContentLinearLayoutManager extends LinearLayoutManager {
+
+	public WrapContentLinearLayoutManager(Context context) {
+		super(context);
+	}
+
+	@Override
+	public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+
+		try {
+			super.onLayoutChildren(recycler, state);
+		} catch (IndexOutOfBoundsException e) {
+			Log.e("RECYCLER VIEW", "OUT INDEX BUG");
+			Crashlytics.log(Log.DEBUG, "RECYCLER VIEW", "OUT INDEX BUG");
+		}
+	}
+}
