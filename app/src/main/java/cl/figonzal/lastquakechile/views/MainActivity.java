0c33dc2ee0a5dfe20148db8f250358ee07b79838
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2019-06-22 00:40:39 -0400

    Dialog de changelog la realizar updates
    Signed-off-by: Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>

--- app/src/main/java/cl/figonzal/lastquakechile/views/MainActivity.java
+++ app/src/main/java/cl/figonzal/lastquakechile/views/MainActivity.java
@@ -1,10 +1,20 @@
 package cl.figonzal.lastquakechile.views;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +37,8 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.util.Objects;
+
 import cl.figonzal.lastquakechile.FragmentPageAdapter;
 import cl.figonzal.lastquakechile.R;
 import cl.figonzal.lastquakechile.services.MyFirebaseMessagingService;
@@ -37,181 +49,215 @@ import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOption
 public class MainActivity extends AppCompatActivity {
 
 
-	private AppBarLayout mAppBarLayout;
-	private ImageView mIvFoto;
+    private AppBarLayout mAppBarLayout;
+    private ImageView mIvFoto;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		//Setear configuracion por defecto
-		PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Setear configuracion por defecto
+        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
 
-		//Checkear si preferencias tiene modo noche
-		QuakeUtils.checkNightMode(this);
+        //Checkear si preferencias tiene modo noche
+        QuakeUtils.checkNightMode(this);
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-
-		Bundle mBundleWelcome = getIntent().getExtras();
-		if (mBundleWelcome != null) {
-			//Si el usuario viene desde deep link, no se realiza first check
-			//Si viene desde Google play, se realiza el check
-			if (!mBundleWelcome.getBoolean(getString(R.string.desde_deep_link))) {
-				QuakeUtils.checkFirstRun(this, false);
-			}
-		}
-
-		//Verifica si el celular tiene googleplay services activado
-		QuakeUtils.checkPlayServices(this);
-
-		//FIREBASE SECTION
-		FirebaseMessaging.getInstance().setAutoInitEnabled(true);
-		FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this,
-				new OnSuccessListener<InstanceIdResult>() {
-					@Override
-					public void onSuccess(InstanceIdResult instanceIdResult) {
-						String token = instanceIdResult.getToken();
-						Log.d(getString(R.string.TAG_FIREBASE_TOKEN), token);
-
-						//CRASH ANALYTICS LOG
-						Crashlytics.log(Log.DEBUG, getString(R.string.TAG_FIREBASE_TOKEN), token);
-						Crashlytics.setUserIdentifier(token);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
-					}
-				});
+        Bundle mBundleWelcome = getIntent().getExtras();
+        if (mBundleWelcome != null) {
+            //Si el usuario viene desde deep link, no se realiza first check
+            //Si viene desde Google play, se realiza el check
+            if (!mBundleWelcome.getBoolean(getString(R.string.desde_deep_link))) {
+                QuakeUtils.checkFirstRun(this, false);
+            }
+        }
 
-		//Llamada a creacion de canal de notificaciones
-		MyFirebaseMessagingService.createNotificationChannel(getApplicationContext());
+        //Verifica si el celular tiene googleplay services activado
+        QuakeUtils.checkPlayServices(this);
 
-		//Buscar toolbar en resources
-		Toolbar mToolbar = findViewById(R.id.tool_bar);
+        //FIREBASE SECTION
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this,
+                new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+                        String token = instanceIdResult.getToken();
+                        Log.d(getString(R.string.TAG_FIREBASE_TOKEN), token);
 
-		//Setear el toolbar sobre el main activity
-		setSupportActionBar(mToolbar);
-
-		//Appbar layout para minimizar el collapse toolbar cuando se presiona el tab de mapa
-		mAppBarLayout = findViewById(R.id.app_bar);
-
-		//View pager para los fragments (Solo 1 fragment en esta app)
-		ViewPager mViewPager = findViewById(R.id.view_pager);
-		mViewPager.setAdapter(new FragmentPageAdapter(getSupportFragmentManager(),
-				getApplicationContext()));
-
-
-		//Seteo de tabs.
-		TabLayout mTabLayout = findViewById(R.id.tabs);
-		mTabLayout.setupWithViewPager(mViewPager);
-		mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-
-			@Override
-			public void onTabSelected(TabLayout.Tab tab) {
-				if (tab.getPosition() == 1) {
-
-					mAppBarLayout.setExpanded(false);
-				} else {
-					mAppBarLayout.setExpanded(true);
-				}
-			}
-
-			@Override
-			public void onTabUnselected(TabLayout.Tab tab) {
-
-			}
-
-			@Override
-			public void onTabReselected(TabLayout.Tab tab) {
-
-			}
-		});
-
-		//Setear collapsing toolbar con titulo estatico superior y animacion de colores al recoger
-		// toolbar
-		CollapsingToolbarLayout mCollapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
-		mCollapsingToolbarLayout.setTitleEnabled(true);
-
-		int modeNightType = getResources().getConfiguration().uiMode &
-				Configuration.UI_MODE_NIGHT_MASK;
-
-		//Detecta modo noche automatico como YES
-		if (modeNightType == Configuration.UI_MODE_NIGHT_YES) {
-			mCollapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorPrimaryNightMode
-					, getTheme()));
-		} else if (modeNightType == Configuration.UI_MODE_NIGHT_NO) {
-			mCollapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorPrimary,
-					getTheme()));
-		}
-
-		//Setear imagen de toolbar con Glide
-		mIvFoto = findViewById(R.id.toolbar_image);
-		loadImage();
-
-		//Suscribir automaticamente al tema (FIREBASE - Quakes)
-		MyFirebaseMessagingService.checkSuscription(this);
+                        //CRASH ANALYTICS LOG
+                        Crashlytics.log(Log.DEBUG, getString(R.string.TAG_FIREBASE_TOKEN), token);
+                        Crashlytics.setUserIdentifier(token);
 
 
-		/*Button button = findViewById(R.id.button2);
+                    }
+                });
 
-		button.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				final DialogFragment dialogFragment = new ChangeLogDialog();
-				dialogFragment.show(getSupportFragmentManager(),"NoticeDialogFragment");
+        //Llamada a creacion de canal de notificaciones
+        MyFirebaseMessagingService.createNotificationChannel(getApplicationContext());
 
-				Button cerrar = dialogFragment.getView().findViewById(R.id.btn_info_accept);
-				cerrar.setOnClickListener(new View.OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						dialogFragment.dismiss();
-					}
-				});
-			}
-		});*/
+        //Buscar toolbar en resources
+        Toolbar mToolbar = findViewById(R.id.tool_bar);
 
+        //Setear el toolbar sobre el main activity
+        setSupportActionBar(mToolbar);
 
+        //Appbar layout para minimizar el collapse toolbar cuando se presiona el tab de mapa
+        mAppBarLayout = findViewById(R.id.app_bar);
 
-	}
-
-	/**
-	 * Funcion encargada de cargar la imagen de fondo en el toolbar
-	 */
-	private void loadImage() {
-		Glide.with(this)
-				.load(R.drawable.foto)
-				.apply(
-						new RequestOptions()
-								.placeholder(R.drawable.placeholder)
-								.error(R.drawable.not_found)
-				)
-				.transition(withCrossFade())
-				.listener(new RequestListener<Drawable>() {
-					@Override
-					public boolean onLoadFailed(@Nullable GlideException e, Object model,
-												Target<Drawable> target,
-												boolean isFirstResource) {
-						mIvFoto.setImageDrawable(getDrawable(R.drawable.not_found));
-						return false;
-					}
-
-					//No es necesario usarlo (If u want)
-					@Override
-					public boolean onResourceReady(Drawable resource, Object model,
-												   Target<Drawable> target,
-												   DataSource dataSource,
-												   boolean isFirstResource) {
-
-						return false;
-					}
-				})
-				.into(mIvFoto);
-	}
+        //View pager para los fragments (Solo 1 fragment en esta app)
+        ViewPager mViewPager = findViewById(R.id.view_pager);
+        mViewPager.setAdapter(new FragmentPageAdapter(getSupportFragmentManager(),
+                getApplicationContext()));
 
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		QuakeUtils.checkPlayServices(this);
-	}
+        //Seteo de tabs.
+        TabLayout mTabLayout = findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 1) {
+
+                    mAppBarLayout.setExpanded(false);
+                } else {
+                    mAppBarLayout.setExpanded(true);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        //Setear collapsing toolbar con titulo estatico superior y animacion de colores al recoger
+        // toolbar
+        CollapsingToolbarLayout mCollapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        mCollapsingToolbarLayout.setTitleEnabled(true);
+
+        int modeNightType = getResources().getConfiguration().uiMode &
+                Configuration.UI_MODE_NIGHT_MASK;
+
+        //Detecta modo noche automatico como YES
+        if (modeNightType == Configuration.UI_MODE_NIGHT_YES) {
+            mCollapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorPrimaryNightMode
+                    , getTheme()));
+        } else if (modeNightType == Configuration.UI_MODE_NIGHT_NO) {
+            mCollapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorPrimary,
+                    getTheme()));
+        }
+
+        //Setear imagen de toolbar con Glide
+        mIvFoto = findViewById(R.id.toolbar_image);
+        loadImage();
+
+        //Suscribir automaticamente al tema (FIREBASE - Quakes)
+        MyFirebaseMessagingService.checkSuscription(this);
+
+        //Mostrar dialog para actualización de versiones
+        change_log_dialog();
+
+    }
+
+    /**
+     * Funcion encargada de cargar la imagen de fondo en el toolbar
+     */
+    private void loadImage() {
+        Glide.with(this)
+                .load(R.drawable.foto)
+                .apply(
+                        new RequestOptions()
+                                .placeholder(R.drawable.placeholder)
+                                .error(R.drawable.not_found)
+                )
+                .transition(withCrossFade())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model,
+                                                Target<Drawable> target,
+                                                boolean isFirstResource) {
+                        mIvFoto.setImageDrawable(getDrawable(R.drawable.not_found));
+                        return false;
+                    }
+
+                    //No es necesario usarlo (If u want)
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model,
+                                                   Target<Drawable> target,
+                                                   DataSource dataSource,
+                                                   boolean isFirstResource) {
+
+                        return false;
+                    }
+                })
+                .into(mIvFoto);
+    }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        QuakeUtils.checkPlayServices(this);
+    }
+
+    private void change_log_dialog() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            long versionCode = packageInfo.versionCode;
+
+            final SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+
+            long actual_version_code = sharedPreferences.getLong("actual_version_code", 0);
+
+            Log.d("VERSION_CODE_APP", String.valueOf(actual_version_code));
+
+            if (actual_version_code == 0) {
+                //Actualizar version
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putLong("actual_version_code", versionCode);
+                editor.apply();
+            } else if (actual_version_code < versionCode) {
+
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.changelog_layout);
+                dialog.setCanceledOnTouchOutside(false);
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+                Button entendido = dialog.findViewById(R.id.btn_changelog_accept);
+                entendido.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                TextView tv_descripcion = dialog.findViewById(R.id.tv_changelog_description);
+                TextView tv_version = dialog.findViewById(R.id.tv_changelog_version);
+
+                tv_version.setText("v1.2.1");
+                tv_descripcion.setText("- Corrección de bug que provocaba cierres inesperados\n" +
+                        "- Actividad de configuración de preferencias\n" +
+                        "- Modo noche\n" +
+                        "- Google map en detalle de sismo\n" +
+                        "- Correcciones de bugs");
+
+                //Actualizar version
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putLong("actual_version_code", versionCode);
+                editor.apply();
+
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
