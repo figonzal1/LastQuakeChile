6a3d2d688f47dbe55385d1f7f1fb307c5d96882d
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2019-07-01 23:19:19 -0400

    Implementacion de limite de sismos mostrados en settings activity, se agregan logs para debug y se corrige bug de intents al cerrar settings activity con back press.
    Signed-off-by: Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>

--- app/src/main/java/cl/figonzal/lastquakechile/SettingsActivity.java
+++ app/src/main/java/cl/figonzal/lastquakechile/SettingsActivity.java
@@ -1,16 +1,19 @@
 package cl.figonzal.lastquakechile;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SeekBarPreference;
 
 import java.util.Objects;
 
@@ -20,166 +23,222 @@ import cl.figonzal.lastquakechile.views.MainActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		QuakeUtils.checkNightMode(this);
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.settings_activity);
-		getSupportFragmentManager()
-				.beginTransaction()
-				.replace(R.id.settings, new SettingsFragment())
-				.commit();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        QuakeUtils.checkNightMode(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_menu, new SettingsFragment())
+                .commit();
 
-		Toolbar mToolbar = findViewById(R.id.tool_bar_settings);
-		setSupportActionBar(mToolbar);
+        Toolbar mToolbar = findViewById(R.id.tool_bar_settings);
+        setSupportActionBar(mToolbar);
 
-		Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setTitle(R.string.settings);
-	}
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.settings);
+    }
 
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		QuakeUtils.checkNightMode(this);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        QuakeUtils.checkNightMode(this);
+    }
 
-	@Override
-	public void onBackPressed() {
-		Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		startActivity(intent);
-		finish();
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
 
-		super.onBackPressed();
-	}
+        super.onBackPressed();
+    }
 
-	public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-		private Activity activity;
+        private Activity activity;
+        private SeekBarPreference seekBarPreference;
+        private SharedPreferences sharedPreferences;
 
-		@Override
-		public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-			setPreferencesFromResource(R.xml.root_preferences, rootKey);
-			if (getActivity() != null) {
-				activity = getActivity();
-			}
-		}
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            if (getActivity() != null) {
+                activity = getActivity();
+                sharedPreferences =
+                        activity.getSharedPreferences(getString(R.string.MAIN_SHARED_PREF_KEY),
+                                Context.MODE_PRIVATE);
+            }
+        }
 
-		@Override
-		public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences,
-											  String key) {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-			//Preferencia modo noche manual
-			if (key.equals(activity.getString(R.string.NIGHT_MODE_MANUAL_KEY))) {
+            /*
+             * SeekBar
+             */
+            seekBarPreference =
+                    findPreference(activity.getString(R.string.SHARED_PREF_LIST_QUAKE_NUMBER));
+            assert seekBarPreference != null;
+            seekBarPreference.setMin(10);
+            seekBarPreference.setMax(30);
 
-				//Si el modo manual esta activado
-				if (sharedPreferences.getBoolean(activity.getString(R.string.NIGHT_MODE_MANUAL_KEY),
-						false)) {
-					AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-					activity.setTheme(R.style.DarkAppTheme);
-					activity.recreate();
+            String limite =
+                    String.valueOf(sharedPreferences.getInt(activity.getString(R.string.SHARED_PREF_LIST_QUAKE_NUMBER), 0));
 
-					//Mostrar toast modo manual activado
-					Toast.makeText(activity.getApplicationContext(),
-							getString(R.string.NIGHT_MODE_MANUAL_KEY_TOAST_ON),
-							Toast.LENGTH_LONG).show();
+            //Setear resumen por defecto
+            if (limite.equals("0")) {
+                seekBarPreference.setValue(15);
+                seekBarPreference.setSummary(String.format(activity.getString(R.string.LIST_QUAKE_NUMBER_SUMMARY), "15"));
+            } else {
+                seekBarPreference.setSummary(String.format(activity.getString(R.string.LIST_QUAKE_NUMBER_SUMMARY), limite));
+            }
+        }
 
-					//Setear automatico como false si manual esta activado
-					SharedPreferences.Editor edit = sharedPreferences.edit();
-					edit.putBoolean(activity.getString(R.string.NIGHT_MODE_AUTO_KEY), false);
-					edit.apply();
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                              String key) {
 
-				}
-				//Si modo manual no esta activado
-				else {
-					AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-					activity.setTheme(R.style.AppTheme);
-					activity.recreate();
+            //Numero de listado de sismos falla si no se usa shared pref con la key principal
+            // (MAIN_SHARED_PREF_KEY)
+            sharedPreferences =
+                    activity.getSharedPreferences(activity.getString(R.string.MAIN_SHARED_PREF_KEY), Context.MODE_PRIVATE);
+            /*
+             * Preferencia Modo Noche
+             * MANUAL
+             */
+            if (key.equals(activity.getString(R.string.NIGHT_MODE_MANUAL_KEY))) {
 
-					//Mostrar toast modo manual desactivado
-					Toast.makeText(activity.getApplicationContext(),
-							activity.getString(R.string.NIGHT_MODE_MANUAL_KEY_TOAST_OFF),
-							Toast.LENGTH_LONG).show();
-				}
-			}
+                //Si el modo manual esta activado
+                if (sharedPreferences.getBoolean(activity.getString(R.string.NIGHT_MODE_MANUAL_KEY),
+                        false)) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    activity.setTheme(R.style.DarkAppTheme);
+                    activity.recreate();
 
-			//MODO AUTOMATICO
-			//Preferencia modo noche automatico
-			else if (key.equals(activity.getString(R.string.NIGHT_MODE_AUTO_KEY))) {
+                    //Mostrar toast modo manual activado
+                    Toast.makeText(activity.getApplicationContext(),
+                            getString(R.string.NIGHT_MODE_MANUAL_KEY_TOAST_ON),
+                            Toast.LENGTH_LONG).show();
 
-				//Si automatico esta activado, preguntar el estado del modo
-				if (sharedPreferences.getBoolean(activity.getString(R.string.NIGHT_MODE_AUTO_KEY),
-						false)) {
-					AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+                    //Setear automatico como false si manual esta activado
+                    SharedPreferences.Editor edit = sharedPreferences.edit();
+                    edit.putBoolean(activity.getString(R.string.NIGHT_MODE_AUTO_KEY), false);
+                    edit.apply();
 
-					//Obtener el estado del modo
-					int modeNightType = activity.getResources().getConfiguration().uiMode &
-							Configuration.UI_MODE_NIGHT_MASK;
+                }
+                //Si modo manual no esta activado
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    activity.setTheme(R.style.AppTheme);
+                    activity.recreate();
 
-					//Detecta modo noche automatico como YES
-					if (modeNightType == Configuration.UI_MODE_NIGHT_YES) {
-						activity.setTheme(R.style.DarkAppTheme);
-					} else if (modeNightType == Configuration.UI_MODE_NIGHT_NO) {
-						activity.setTheme(R.style.AppTheme);
-					}
-					activity.recreate();
+                    //Mostrar toast modo manual desactivado
+                    Toast.makeText(activity.getApplicationContext(),
+                            activity.getString(R.string.NIGHT_MODE_MANUAL_KEY_TOAST_OFF),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
 
-					//Mostrar toast con mensaje de modo noche automatico activado
-					Toast.makeText(activity.getApplicationContext(),
-							activity.getString(R.string.NIGHT_MODE_AUTO_KEY_TOAST_ON),
-							Toast.LENGTH_LONG).show();
-					//Setear automatico como false si manual esta activado
-					SharedPreferences.Editor edit = sharedPreferences.edit();
-					edit.putBoolean(activity.getString(R.string.NIGHT_MODE_MANUAL_KEY), false);
-					edit.apply();
-				}
-				//Si auto esta desactivado, tema claro por defecto
-				else {
-					AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-					activity.setTheme(R.style.AppTheme);
-					activity.recreate();
+            /*
+             * Preferencias de Modo Noche
+             * AUTOMATICO
+             */
+            else if (key.equals(activity.getString(R.string.NIGHT_MODE_AUTO_KEY))) {
 
-					//Mostrar toast con mensaje de modo noche automatico desactivado
-					Toast.makeText(activity.getApplicationContext(),
-							activity.getString(R.string.NIGHT_MODE_AUTO_KEY_TOAST_OFF),
-							Toast.LENGTH_LONG).show();
-				}
-			}
+                //Si automatico esta activado, preguntar el estado del modo
+                if (sharedPreferences.getBoolean(activity.getString(R.string.NIGHT_MODE_AUTO_KEY),
+                        false)) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
 
-			//PREFERENCIA DE ALERTAS
-			if (key.equals(activity.getString(R.string.FIREBASE_PREF_KEY))) {
-				MyFirebaseMessagingService.checkSuscription(activity);
+                    //Obtener el estado del modo
+                    int modeNightType = activity.getResources().getConfiguration().uiMode &
+                            Configuration.UI_MODE_NIGHT_MASK;
 
-				//Si el switch esta ON, lanzar toast con SUSCRITO
-				if (sharedPreferences.getBoolean(activity.getString(R.string.FIREBASE_PREF_KEY),
-						true)) {
-					Toast.makeText(getContext(),
-							activity.getString(R.string.FIREBASE_PREF_KEY_TOAST_ALERTAS_ON),
-							Toast.LENGTH_LONG).show();
+                    //Detecta modo noche automatico como YES
+                    if (modeNightType == Configuration.UI_MODE_NIGHT_YES) {
+                        activity.setTheme(R.style.DarkAppTheme);
+                    } else if (modeNightType == Configuration.UI_MODE_NIGHT_NO) {
+                        activity.setTheme(R.style.AppTheme);
+                    }
+                    activity.recreate();
 
-				}
-				//Si el switch esta off lanzar toast con NO SUSCRITO
-				else {
-					Toast.makeText(getContext(),
-							activity.getString(R.string.FIREBASE_PREF_KEY_TOAST_ALERTAS_OFF),
-							Toast.LENGTH_LONG).show();
-				}
+                    //Mostrar toast con mensaje de modo noche automatico activado
+                    Toast.makeText(activity.getApplicationContext(),
+                            activity.getString(R.string.NIGHT_MODE_AUTO_KEY_TOAST_ON),
+                            Toast.LENGTH_LONG).show();
+                    //Setear automatico como false si manual esta activado
+                    SharedPreferences.Editor edit = sharedPreferences.edit();
+                    edit.putBoolean(activity.getString(R.string.NIGHT_MODE_MANUAL_KEY), false);
+                    edit.apply();
+                }
+                //Si auto esta desactivado, tema claro por defecto
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    activity.setTheme(R.style.AppTheme);
+                    activity.recreate();
 
-			}
+                    //Mostrar toast con mensaje de modo noche automatico desactivado
+                    Toast.makeText(activity.getApplicationContext(),
+                            activity.getString(R.string.NIGHT_MODE_AUTO_KEY_TOAST_OFF),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
 
-		}
+            /*
+             * Preferencias de alertas
+             */
+            if (key.equals(activity.getString(R.string.FIREBASE_PREF_KEY))) {
+                MyFirebaseMessagingService.checkSuscription(activity);
 
-		@Override
-		public void onResume() {
-			super.onResume();
-			getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-		}
+                //Si el switch esta ON, lanzar toast con SUSCRITO
+                if (sharedPreferences.getBoolean(activity.getString(R.string.FIREBASE_PREF_KEY),
+                        true)) {
+                    Toast.makeText(getContext(),
+                            activity.getString(R.string.FIREBASE_PREF_KEY_TOAST_ALERTAS_ON),
+                            Toast.LENGTH_LONG).show();
 
-		@Override
-		public void onPause() {
-			super.onPause();
-			getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-		}
-	}
+                }
+                //Si el switch esta off lanzar toast con NO SUSCRITO
+                else {
+                    Toast.makeText(getContext(),
+                            activity.getString(R.string.FIREBASE_PREF_KEY_TOAST_ALERTAS_OFF),
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            /*
+             * Preferencias de numero de sismos
+             */
+            if (key.equals(activity.getString(R.string.SHARED_PREF_LIST_QUAKE_NUMBER))) {
+
+                seekBarPreference.setSummary(String.format(activity.getString(R.string.LIST_QUAKE_NUMBER_SUMMARY), String.valueOf(seekBarPreference.getValue())));
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt(activity.getString(R.string.SHARED_PREF_LIST_QUAKE_NUMBER),
+                        seekBarPreference.getValue());
+                editor.apply();
+
+                Log.e(activity.getString(R.string.TAG_FRAGMENT_SETTINGS),
+                        String.format(activity.getString(R.string.TAG_FRAGMENT_SETTINGS_QUAKE_LIST_LIMIT), seekBarPreference.getValue()));
+            }
+
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+    }
 }
