decc3c6c3f4a8c13707778231a73313ee21f26b7
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2020-03-17 17:52:30 -0300

    Correcciones en test instrumentales
    Signed-off-by: figonzal1 <Felipe.gonzalezalarcon94@gmail.com>

--- app/src/androidTest/java/cl/figonzal/lastquakechile/views/settings/NightModePrefTest.java
+++ app/src/androidTest/java/cl/figonzal/lastquakechile/views/settings/NightModePrefTest.java
@@ -12,6 +12,7 @@ import androidx.test.rule.ActivityTestRule;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -39,77 +40,47 @@ import static org.hamcrest.core.IsNot.not;
 @LargeTest
 public class NightModePrefTest {
 
-	@Rule
-	public final ActivityTestRule<SettingsActivity> testRule =
-			new ActivityTestRule<>(SettingsActivity.class);
+    @Rule
+    public final ActivityTestRule<SettingsActivity> testRule =
+            new ActivityTestRule<>(SettingsActivity.class);
 
-	private Activity mActivity;
+    private Activity mActivity;
 
-	private static Matcher<View> childAtPosition(
-			final Matcher<View> parentMatcher, final int position) {
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
 
-		return new TypeSafeMatcher<View>() {
-			@Override
-			public void describeTo(Description description) {
-				description.appendText("Child at position " + position + " in parent ");
-				parentMatcher.describeTo(description);
-			}
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
 
-			@Override
-			public boolean matchesSafely(View view) {
-				ViewParent parent = view.getParent();
-				return parent instanceof ViewGroup && parentMatcher.matches(parent)
-						&& view.equals(((ViewGroup) parent).getChildAt(position));
-			}
-		};
-	}
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 
-	@Before
-	public void setup() {
-		mActivity = testRule.getActivity();
-	}
+    @Before
+    public void setup() {
+        mActivity = testRule.getActivity();
+    }
 
+    @Test
+    public void test1_check_night_mode_automatic_off() {
 
-	@Test
-    public void test1_check_night_mode_manual_on() {
-
-		ViewInteraction switchManual = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.recycler_view),
-								childAtPosition(
-										withId(android.R.id.list_container),
-										0)),
-						5),
-						isDisplayed()));
-
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		//Realizar click
-		switchManual.perform(click());
-
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		//Checkear display de Toast de shared pref TRUE
-		onView(withText(R.string.NIGHT_MODE_MANUAL_KEY_TOAST_ON)).inRoot(withDecorView(not(is(mActivity.getWindow().getDecorView()))))
-				.check(matches(isDisplayed()));
-
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-    public void test2_check_night_mode_manual_off() {
+        ViewInteraction switchAuto = onView(
+                Matchers.allOf(childAtPosition(
+                        Matchers.allOf(withId(R.id.recycler_view),
+                                childAtPosition(
+                                        withId(android.R.id.list_container),
+                                        0)),
+                        6),
+                        isDisplayed()));
 
         try {
             Thread.sleep(2000);
@@ -117,40 +88,143 @@ public class NightModePrefTest {
             e.printStackTrace();
         }
 
-		ViewInteraction switchManual = onView(
+        //Realizar click
+        switchAuto.perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Checkear display de Toast de shared pref TRUE
+        onView(withText(R.string.NIGHT_MODE_AUTO_KEY_TOAST_OFF)).inRoot(withDecorView(not(is(mActivity.getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test2_check_night_mode_manual_on() {
+
+        ViewInteraction switchManual = onView(
                 allOf(childAtPosition(
                         allOf(withId(R.id.recycler_view),
-								childAtPosition(
-										withId(android.R.id.list_container),
-										0)),
-						5),
-						isDisplayed()));
+                                childAtPosition(
+                                        withId(android.R.id.list_container),
+                                        0)),
+                        5),
+                        isDisplayed()));
 
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-		//Realizar click
-		switchManual.perform(click());
+        //Realizar click
+        switchManual.perform(click());
 
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-		//Checkear display de Toast de shared pref TRUE
-		onView(withText(R.string.NIGHT_MODE_MANUAL_KEY_TOAST_OFF)).inRoot(withDecorView(not(is(mActivity.getWindow().getDecorView()))))
-				.check(matches(isDisplayed()));
+        //Checkear display de Toast de shared pref TRUE
+        onView(withText(R.string.NIGHT_MODE_MANUAL_KEY_TOAST_ON)).inRoot(withDecorView(not(is(mActivity.getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
 
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void test3_check_night_mode_manual_off() {
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction switchManual = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.recycler_view),
+                                childAtPosition(
+                                        withId(android.R.id.list_container),
+                                        0)),
+                        5),
+                        isDisplayed()));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Realizar click
+        switchManual.perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Checkear display de Toast de shared pref TRUE
+        onView(withText(R.string.NIGHT_MODE_MANUAL_KEY_TOAST_OFF)).inRoot(withDecorView(not(is(mActivity.getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test4_check_night_mode_automatic_on() {
+
+        ViewInteraction switchAuto = onView(
+                Matchers.allOf(childAtPosition(
+                        Matchers.allOf(withId(R.id.recycler_view),
+                                childAtPosition(
+                                        withId(android.R.id.list_container),
+                                        0)),
+                        6),
+                        isDisplayed()));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //Realizar click
+        switchAuto.perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Checkear display de Toast de shared pref TRUE
+        onView(withText(R.string.NIGHT_MODE_AUTO_KEY_TOAST_ON)).inRoot(withDecorView(not(is(mActivity.getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
