3c5b3cf77108cb7deba7019cc017e7d23a5f78a1
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2020-06-28 17:34:58 -0400

    Primeros avances con el change log activity
    Signed-off-by: figonzal1 <Felipe.gonzalezalarcon94@gmail.com>

---
+++ app/src/main/java/cl/figonzal/lastquakechile/ChangeLogActivity.java
@@ -0,0 +1,14 @@
+package cl.figonzal.lastquakechile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangeLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_log);
+    }
+}
