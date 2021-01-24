95c3ca704d9494f96651e995fc8b684422290b08
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2021-01-24 00:52:41 -0300

    Cambios en repositorios
    Took 50 minutes

--- app/src/main/java/cl/figonzal/lastquakechile/repository/ReportRepository.java
+++ app/src/main/java/cl/figonzal/lastquakechile/repository/ReportRepository.java
@@ -1,6 +1,6 @@
 package cl.figonzal.lastquakechile.repository;
 
-import android.app.Application;
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
@@ -29,7 +29,7 @@ public class ReportRepository implements NetworkRepository<ReportModel> {
 
     private static final String TAG_GET_REPORTS = "ListadoReportes";
     private static ReportRepository instance;
-    private final Application mApplication;
+    private final Context appContext;
 
     //REPORTES
     private List<ReportModel> reportModelList = new ArrayList<>();
@@ -37,14 +37,14 @@ public class ReportRepository implements NetworkRepository<ReportModel> {
     private final MutableLiveData<Boolean> isLoadingReports = new MutableLiveData<>();
     private final SingleLiveEvent<String> responseMsgErrorList = new SingleLiveEvent<>();
 
-    private ReportRepository(Application application) {
-        this.mApplication = application;
+    private ReportRepository(Context appContext) {
+        this.appContext = appContext;
     }
 
-    public static ReportRepository getIntance(Application application) {
+    public static ReportRepository getIntance(Context appContext) {
 
         if (instance == null) {
-            instance = new ReportRepository(application);
+            instance = new ReportRepository(appContext);
         }
         return instance;
     }
@@ -92,29 +92,29 @@ public class ReportRepository implements NetworkRepository<ReportModel> {
 
             if (error instanceof TimeoutError) {
 
-                Timber.e(mApplication.getString(R.string.TAG_VOLLEY_ERROR_TIMEOUT));
+                Timber.e(appContext.getString(R.string.TAG_VOLLEY_ERROR_TIMEOUT));
 
-                responseMsgErrorList.postValue(mApplication.getString(R.string.VIEWMODEL_TIMEOUT_ERROR));
+                responseMsgErrorList.postValue(appContext.getString(R.string.VIEWMODEL_TIMEOUT_ERROR));
             }
 
             //Error de conexion a internet
             else if (error instanceof NetworkError) {
 
-                Timber.e(mApplication.getString(R.string.TAG_VOLLEY_ERROR_NETWORK));
+                Timber.e(appContext.getString(R.string.TAG_VOLLEY_ERROR_NETWORK));
 
-                responseMsgErrorList.postValue(mApplication.getString(R.string.VIEWMODEL_NOCONNECTION_ERROR));
+                responseMsgErrorList.postValue(appContext.getString(R.string.VIEWMODEL_NOCONNECTION_ERROR));
             }
 
             //Error de servidor
             else if (error instanceof ServerError) {
 
-                Timber.e(mApplication.getString(R.string.TAG_VOLLEY_ERROR_SERVER));
+                Timber.e(appContext.getString(R.string.TAG_VOLLEY_ERROR_SERVER));
 
-                responseMsgErrorList.postValue(mApplication.getString(R.string.VIEWMODEL_SERVER_ERROR));
+                responseMsgErrorList.postValue(appContext.getString(R.string.VIEWMODEL_SERVER_ERROR));
             }
         };
 
-        String url = mApplication.getString(R.string.URL_GET_PROD_REPORTS);
+        String url = appContext.getString(R.string.URL_GET_PROD_REPORTS);
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, listener, errorListener);
         isLoadingReports.postValue(true);
@@ -124,6 +124,6 @@ public class ReportRepository implements NetworkRepository<ReportModel> {
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
-        VolleySingleton.getInstance(mApplication).addToRequestQueue(stringRequest, TAG_GET_REPORTS);
+        VolleySingleton.getInstance(appContext).addToRequestQueue(stringRequest, TAG_GET_REPORTS);
     }
 }
