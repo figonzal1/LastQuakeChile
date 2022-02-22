613f553aa726fe24497a4939b96db5fa09c3e976
Felipe Gonzalez Alarcon <felipe.gonzalezalarcon94@gmail.com>
2022-02-21 22:49:10 -0300

    Implementacion de Ads al abrir la aplicacion suspendida
    Signed-off-by: Felipe Gonzalez Alarcon <felipe.gonzalezalarcon94@gmail.com>
    
    Took 9 minutes

--- app/src/main/java/cl/figonzal/lastquakechile/core/ApplicationController.kt
+++ app/src/main/java/cl/figonzal/lastquakechile/core/ApplicationController.kt
@@ -4,8 +4,10 @@ import android.app.Application
 import cl.figonzal.lastquakechile.BuildConfig
 import cl.figonzal.lastquakechile.core.di.appModule
 import cl.figonzal.lastquakechile.core.di.networkModule
+import cl.figonzal.lastquakechile.core.services.AppOpenService
 import cl.figonzal.lastquakechile.quake_feature.di.quakeModule
 import cl.figonzal.lastquakechile.reports_feature.di.reportModule
+import com.google.android.gms.ads.MobileAds
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -13,7 +15,12 @@ import org.koin.core.logger.Level
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
+
 class ApplicationController : Application() {
+
+    companion object {
+        lateinit var appOpenService: AppOpenService
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -34,5 +41,9 @@ class ApplicationController : Application() {
             BuildConfig.DEBUG -> Timber.plant(DebugTree())
             else -> Timber.plant(CrashlyticsTree())
         }
+
+        MobileAds.initialize(this) { }
+
+        appOpenService = AppOpenService(this)
     }
 }
