7d9bff1ad4f552badc9be6376b61251275bb27f5
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-09-07 23:28:19 -0400

    espresso downgrade
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- app/src/androidTest/java/cl/figonzal/lastquakechile/core/ui/SettingsActivityTest.kt
+++ app/src/androidTest/java/cl/figonzal/lastquakechile/core/ui/SettingsActivityTest.kt
@@ -346,7 +346,7 @@ class SettingsActivityTest {
         }
 
         public override fun matchesSafely(root: Root): Boolean {
-            val type = root.windowLayoutParams2.type
+            val type = root.windowLayoutParams.get().type
             if (type == WindowManager.LayoutParams.TYPE_TOAST) {
                 val windowToken = root.decorView.windowToken
                 val appToken = root.decorView.applicationWindowToken
